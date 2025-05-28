2ccd8bfd43fc8dae9ca9d17c8fdbe145ecf135d8
Mom Jon <momjon@Moms-iMac.attlocal.net>
2025-05-28 08:22:02 -0400

    spine2

--- app/studio/layout.tsx
+++ app/studio/layout.tsx
@@ -4,11 +4,8 @@ import '../globals.css'
 
 export const metadata = {
   title: 'Studio',
-}
-
-export const viewport = {
-  width: 'device-width',
-  initialScale: 1,
+  description:'Sanity Content Studio',
+  viewport:'width=device-width, initialScale=1',
 }
 
 export default function StudioLayout({
