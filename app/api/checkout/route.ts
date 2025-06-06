09c16b258fb49b796484bdd797b305889ec988f5
Mom Jon <momjon@Moms-iMac.attlocal.net>
2025-06-06 18:22:06 -0400

    spine8888

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: NextRequest) {
         product_data: {
           name: `${item.title} - Size ${item.size}`,
           metadata: {
-            productId: item.productId,
+            productId: item.Id,
             size: item.size,
           },
         },
